6e551d244e93694c654d5961f29abf9d4b627e7e
hwna00 <layton3534@gmail.com>
2023-07-05 14:18:16 +0900

    3.3.2 정적 파일과 뷰

--- ch03/meadowlark.js
+++ ch03/meadowlark.js
@@ -3,6 +3,8 @@ const expressHandlebars = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/public"));
+
 app.engine(
   "handlebars",
   expressHandlebars({
