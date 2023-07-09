d57fe40e8f191c59b64798861e87495637caa06c
hwna00 <layton3534@gmail.com>
2023-07-10 01:13:52 +0900

    8.5 익스프레스를 이요한 폼 처리

--- ch08/lib/handlers.js
+++ ch08/lib/handlers.js
@@ -9,6 +9,20 @@ exports.sectionTest = (req, res) => {
   res.render("section-test");
 };
 
+exports.newsletterSignup = (req, res) => {
+  res.render("newsletter-signup", { csrf: "CSRF token goes here" });
+};
+
+exports.newsletterSignupProcess = (req, res) => {
+  console.log("CSRF token (from hidden form field): " + req.body._csrf);
+  console.log("Name (from visible form field): " + req.body.name);
+  console.log("Email (from visible form field): " + req.body.email);
+  res.redirect(303, "/newsletter-signup/thank-you");
+};
+
+exports.newsletterSignupThankYou = (req, res) =>
+  res.render("newsletter-signup-thank-you");
+
 exports.notFound = (req, res) => res.render("404");
 
 // eslint-disable-next-line no-unused-vars
--- ch08/meadowlark.js
+++ ch08/meadowlark.js
@@ -31,6 +31,10 @@ app.get("/about", (req, res) => handlers.about(req, res));
 
 app.get("/section-test", handlers.sectionTest);
 
+app.get("/newsletter-signup", handlers.newsletterSignup);
+app.post("/newsletter-signup/process", handlers.newsletterSignupProcess);
+app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
+
 app.use(handlers.notFound);
 app.use(handlers.serverError);
 
