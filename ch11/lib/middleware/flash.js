21ead489e0bd963ee8c09dc613146fb6fc4876a4
hwna00 <layton3534@gmail.com>
2023-07-12 12:43:01 +0900

    ch11 SetUp

---
+++ ch11/lib/middleware/flash.js
@@ -0,0 +1,7 @@
+module.exports = (req, res, next) => {
+  // console.log(res.locals.flash);
+  // console.log(res.session);
+  // res.locals.flash = req.session.flash;
+  // delete req.session.flash;
+  next();
+};
