b4fe78d8923b535da5e1d5d818bcf4440dd76c83
hwna00 <layton3534@gmail.com>
2023-07-17 15:20:09 +0900

    16.4.5 서버로 정보 전송

--- ch16/client/src/Vacation.js
+++ ch16/client/src/Vacation.js
@@ -1,6 +1,67 @@
 import React, { useEffect, useState } from "react";
 
-function Vacation() {
+function NotifyWhenInSeason({ sku }) {
+  const [registerEmail, setRegisterEmail] = useState(null);
+  const [email, setEmail] = useState("");
+
+  function onSubmit(event) {
+    event.preventDefault();
+    fetch(`http://localhost:3033/api/vacation/${sku}/notify-when-in-season`, {
+      method: "POST",
+      body: JSON.stringify({ email }),
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => {
+      if (res.status < 200 || res.status > 299) {
+        console.log(res);
+        return alert("We had a problem processing this... Please try again");
+      }
+      setRegisterEmail(email);
+    });
+  }
+
+  if (registerEmail) {
+    return (
+      <p>
+        You will notified at {registerEmail} when this vacation is back in
+        season!
+      </p>
+    );
+  }
+
+  return (
+    <>
+      Notify me when this vacation is in season:
+      <input
+        type="email"
+        placeholder="(your email)"
+        value={email}
+        onChange={({ target: { value } }) => setEmail(value)}
+      />
+      <button onClick={onSubmit}>OK</button>
+    </>
+  );
+}
+
+function Vacation({ vacation }) {
+  return (
+    <div
+      key={vacation.sku}
+      style={{ border: "1px solid black", margin: "16px 0px", padding: "16px" }}
+    >
+      <h3>{vacation.name}</h3>
+      <p>{vacation.description}</p>
+      <span className="price">{vacation.price}</span>
+      {!vacation.inSeason && (
+        <div>
+          <p>This vacation is not currently in season.</p>
+          <NotifyWhenInSeason sku={vacation.sku} />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function Vacations() {
   const [vacations, setVacations] = useState([]);
 
   useEffect(() => {
@@ -14,15 +75,11 @@ function Vacation() {
       <h2>Vacations</h2>
       <div className="vacations">
         {vacations.map((vacation) => (
-          <div key={vacation.sku}>
-            <h3>{vacation.name}</h3>
-            <p>{vacation.description}</p>
-            <span className="price">{vacation.price}</span>
-          </div>
+          <Vacation key={vacation.sku} vacation={vacation} />
         ))}
       </div>
     </>
   );
 }
 
-export default Vacation;
+export default Vacations;
