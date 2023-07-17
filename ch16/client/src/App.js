5f6d7f401058d78f99dafcb606c904bb6bc2a1fa
hwna00 <layton3534@gmail.com>
2023-07-17 13:52:24 +0900

    16.4.1 홈페이지

--- ch16/client/src/App.js
+++ ch16/client/src/App.js
@@ -1,7 +1,17 @@
+import logo from "./img/logo.png";
+
+function Home() {
+  return "Home";
+}
+
 function App() {
   return (
-    <div className="App">
-      <h1>HI</h1>
+    <div className="container">
+      <header>
+        <h1>Meadowlark Travel</h1>
+        <img src={logo} alt="" Meadowlark Travel Logo />
+      </header>
+      <Home />
     </div>
   );
 }
