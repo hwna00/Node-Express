a844b9ba7fe4c693f6cd85ac683c58a09e5e7bb3
hwna00 <layton3534@gmail.com>
2023-07-17 14:13:49 +0900

    16.4.2 라우팅

--- ch16/client/src/App.js
+++ ch16/client/src/App.js
@@ -1,18 +1,42 @@
 import logo from "./img/logo.png";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Home() {
-  return "Home";
+  return (
+    <div>
+      <h2>Welcome to Meadowlark Travel</h2>
+      <p>
+        Checkout our <Link to={"/about"}>about</Link> page!
+      </p>
+    </div>
+  );
+}
+
+function About() {
+  return "About";
+}
+
+function NotFound() {
+  return "NotFound";
 }
 
 function App() {
   return (
-    <div className="container">
-      <header>
-        <h1>Meadowlark Travel</h1>
-        <img src={logo} alt="" Meadowlark Travel Logo />
-      </header>
-      <Home />
-    </div>
+    <Router>
+      <div className="container">
+        <header>
+          <h1>Meadowlark Travel</h1>
+          <Link to={"/"}>
+            <img src={logo} alt="" Meadowlark Travel Logo />
+          </Link>
+        </header>
+      </div>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" exact element={<About />} />
+        <Route element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
